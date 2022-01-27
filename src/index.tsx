104899f8d973d78b1fa77a2dc6fd907a4e8ca8bb
Stanislav Bedunkevich <stas.bedunkevich@gmail.com>
2022-01-27 13:09:54 +0100

    v1.0.2

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 export const Greeter = (name: string) => `Hello ${name}`;
 
@@ -7,17 +8,23 @@ type ButtonProps = {
   onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
 
+const StyledButton = styled.button`
+  padding: 5px 10px;
+  background: ${({ theme }) => theme?.primaryColor || 'black'};
+  color: white;
+`;
+
 export const Button: React.FC<ButtonProps> = ({ title, onClick }) => {
   const [counter, setCounter] = React.useState(1);
 
   return (
-    <button
-      onClick={(event) => {
+    <StyledButton
+      onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         setCounter((prev) => prev + 1);
         onClick(event);
       }}
     >
       {title} {counter}
-    </button>
+    </StyledButton>
   );
 };
