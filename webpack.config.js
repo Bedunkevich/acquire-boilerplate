c39cfc7bb2f4278b7779d5fc74d39287e3952a47
Stanislav Bedunkevich <stas.bedunkevich@gmail.com>
2022-01-31 13:45:34 +0100

    v1.0.5

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index',
-  mode: 'development',
-  target: 'web',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    port: 3001,
-  },
-  devtool: 'source-map',
-  output: {
-    publicPath: 'auto',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
+module.exports = ({ WEBPACK_SERVE }) => {
+  const isEnvDevelopment = Boolean(WEBPACK_SERVE);
+
+  return {
+    entry: './src/index',
+    mode: 'development',
+    target: 'web',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
       },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+      port: 3001,
+    },
+    devtool: 'source-map',
+    output: {
+      clean: true,
+      publicPath: 'auto',
+      path: path.resolve(__dirname, 'lib', 'bundle'),
+      filename: 'bundle.js',
+      library: 'Acquire',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [...(isEnvDevelopment ? [new HtmlWebpackPlugin({ template: './public/index.html' })] : [])],
+  };
 };
